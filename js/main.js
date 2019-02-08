692cf754fbed592e06bf4864551adfda2d344da8
Fazliddin Karimov <fazliddinkarimov1701@gmail.com>
2019-02-08 16:28:43 +0500

    this is the new version of search project

--- js/main.js
+++ js/main.js
@@ -13,5 +13,13 @@ btn.addEventListener('click' , function(){
 	let inpVal = document.getElementById('search').value;
 	let result = myData.filter(item => item.name === inpVal);
 	name.innerHTML = result[0].name;
-	
+	document.getElementById('city').innerHTML = result[0].address.city;
+	document.getElementById('street').innerHTML = result[0].address.street;
+	document.getElementById('suite').innerHTML = result[0].address.suite;
+	document.getElementById('zip_code').innerHTML = result[0].address.zipcode;
+	document.getElementById('company').innerHTML = result[0].company.name;
+	document.getElementById('email').innerHTML = result[0].email;
+	document.getElementById('username').innerHTML = result[0].username;
+	document.getElementById('site').innerHTML = result[0].website;
+	document.getElementById('phone').innerHTML = result[0].phone;
 })
